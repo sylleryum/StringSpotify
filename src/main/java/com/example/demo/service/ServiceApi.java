864128dfb5dd7cc9c8be7ebfa8014333486c5961
sylleryum <alisson_piuco@yahoo.com.br>
2019-07-22 22:58:05 -0300

    https%3A%2F%2Fstringspotify.herokuapp.com%2F

--- src/main/java/com/example/demo/service/ServiceApi.java
+++ src/main/java/com/example/demo/service/ServiceApi.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface ServiceApi {
-    String AUTHORIZE_URL = "https://accounts.spotify.com/authorize?client_id=3bcd3f05fa0b45d9a8168faf23ab8729&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fcallback&scope=user-read-private%20user-read-email%20playlist-read-private%20playlist-modify-public%20playlist-modify-private";
+    String AUTHORIZE_URL = "https://accounts.spotify.com/authorize?client_id=3bcd3f05fa0b45d9a8168faf23ab8729&response_type=code&redirect_uri=https%3A%2F%2Fstringspotify.herokuapp.com%2Fcallback&scope=user-read-private%20user-read-email%20playlist-read-private%20playlist-modify-public%20playlist-modify-private";
     String GET_ACCESS = "https://accounts.spotify.com/api/token/";
     String AUTHORIZATION_TO_ACCESS = "Basic M2JjZDNmMDVmYTBiNDVkOWE4MTY4ZmFmMjNhYjg3Mjk6Yzc4YWI5YzE5YmRhNGY3MzgwMDA5M2E3NDdiMmJjODg=";
     String USER_DETAILS = "https://api.spotify.com/v1/me/";
